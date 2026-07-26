03189a3f682e325c95f437cdee2e95a9b85c5986
Sharva <motegaonkarsharva@gmail.com>
2026-07-27 01:16:29 +0530

    Add files via upload

---
+++ 4Introduction.py
@@ -0,0 +1,34 @@
+"""WHAT IS THE INTERNET
+1)whats internet?
+->interconnected network of computers from around the world
+
+2) what's a server?
+-> server is a divice which returns data to clients through internet and it's online 24*7
+
+3)what's a client?
+-> a computer which request's data is called client
+
+4)ip adress?
+->every divice has a ip adress it means all divices are trackable through internet
+
+5)How does internet exactly work?
+->
+1)imagine a client it google,amazon; it sends req to ISP (internet service provider) ie companies like airtel and jio; then isp goes to DNS(Domain name system) it's basically like a adress book
+(adress book basically logo ke nam store krte thi) but for domains and their server's IP adress ka data.domain name is the name of the website(eg-amazom.in)
+
+2)ISP asks DNS about IP adress of domain and transfers it to the client. now the client communicates directly with it's server. we can also open website just by searching it's IP inster of domain.
+
+"""
+
+"""WHAT IS WEB DEVELOPMENT
+1)what is web development?
+->making websites for comapnies
+
+2) when we open a website what actually happens?
+->basically client(apan) amazon ko request bhegta hai( through the process we saw earlier) then server client ko code bhegta hai aur browser uss code ko display kr deta hai
+
+3) HTML->structure
+CSS-> style
+Javascript-> functionality
+
+"""
